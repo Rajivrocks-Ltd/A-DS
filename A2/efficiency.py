3a16f47b1075ffea80717a0a9c0dce05643fe76d
Rajivrocks <y.jethoe@hotmail.com>
2023-04-13 00:19:43 +0200

    Updated test

--- A2/efficiency.py
+++ A2/efficiency.py
@@ -4,7 +4,7 @@ import typing
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+counter = 0
 def naive_linear_scan(grid, value):
     num_cells_searched = 0
     for y, row in enumerate(grid):
@@ -16,65 +16,71 @@ def naive_linear_scan(grid, value):
 
 
 def div_con_search(value: int, x_from: int, x_to: int, y_from: int, y_to: int,
-                   search_grid: typing.List[typing.List[int]]) -> typing.Tuple[int, int, int]:
+                   search_grid: typing.List[typing.List[int]], countvar: int) -> typing.Tuple[int, int, int]:
 
     # Check if the search range is valid
     if x_from > x_to or y_from > y_to:
-        return None, None, 0
+        return None, None, countvar
 
     # Check if the range contains zero elements
     if x_from == x_to and y_from == y_to:
+        countvar += 1
         if search_grid[y_from][x_from] == value:
-            return (y_from, x_from, 1)
+            return (y_from, x_from, countvar)
         else:
-            return None, None, 0
+            return None, None, countvar
 
     mid_x = (x_from + x_to) // 2
     mid_y = (y_from + y_to) // 2
 
     # Check if the middle element matches the search value
     mid_val = search_grid[mid_y][mid_x]
+    countvar += 1
     if mid_val == value:
-        return (mid_y, mid_x, 1)
+        return (mid_y, mid_x, countvar)
 
     if mid_val > value:
         # Recursively search the top-left quadrant
-        y, x, count_tl = div_con_search(value, x_from, mid_x, y_from, mid_y, search_grid)
+        y, x, count_tl = div_con_search(value, x_from, mid_x, y_from, mid_y, search_grid, countvar)
         if all((y,x)):
             return (y , x, count_tl + 1)
 
         # Recursively search the top-right quadrant
-        y, x, count_tr = div_con_search(value, mid_x + 1, x_to, y_from, mid_y, search_grid)
+        y, x, count_tr = div_con_search(value, mid_x + 1, x_to, y_from, mid_y, search_grid, countvar)
         if all((y,x)):
             return (y, x, count_tr + 1)
 
         # Recursively search the bottom-left quadrant
-        y, x, count_bl = div_con_search(value, x_from, mid_x, mid_y + 1, y_to, search_grid)
+        y, x, count_bl = div_con_search(value, x_from, mid_x, mid_y + 1, y_to, search_grid, countvar)
         if all((y,x)):
             return (y, x, count_bl + 1)
 
-        count = count_tl + count_tr + count_bl
+        countvar += count_tl
+        countvar += count_tr
+        countvar += count_bl
 
     else:
         # Recursively search the top-right quadrant
-        y, x, count_tr = div_con_search(value, mid_x + 1, x_to, y_from, mid_y, search_grid)
+        y, x, count_tr = div_con_search(value, mid_x + 1, x_to, y_from, mid_y, search_grid, countvar)
         if all((y,x)):
             return (y, x, count_tr + 1)
 
         # Recursively search the bottom-left quadrant
-        y, x, count_bl = div_con_search(value, x_from, mid_x, mid_y + 1, y_to, search_grid)
+        y, x, count_bl = div_con_search(value, x_from, mid_x, mid_y + 1, y_to, search_grid, countvar)
         if all((y,x)):
             return (y, x, count_bl + 1)
 
         # Check if the search value is in the bottom-right quadrant
-        y, x, count_br = div_con_search(value, mid_x + 1, x_to, mid_y + 1, y_to, search_grid)
+        y, x, count_br = div_con_search(value, mid_x + 1, x_to, mid_y + 1, y_to, search_grid, countvar)
         if all((y,x)):
             return (y, x, count_br + 1)
 
-        count = count_br + count_bl + count_tr
+        countvar += count_br
+        countvar += count_bl
+        countvar += count_tr
 
     # If the search value is not found in any quadrant, return None
-    return None, None, count
+    return None, None, countvar
 
 
 def generate_grid(size):
@@ -82,13 +88,14 @@ def generate_grid(size):
     return grid
 
 
-grid_sizes = [3, 10, 50, 100, 200, 400, 800, 1600, 3200, 6400, 9600, 19200]
+grid_sizes = [5,10,100,200,400,800,1600,3200,6400,9600,19200,38400]
 linear_scan_cells = []
 divconq_cells = []
 linear_scan_times = []
 divconq_times = []
 
 for size in grid_sizes:
+    counter = 0
     grid = generate_grid(size)
     search_value = grid[-1][-1]
 
@@ -99,13 +106,16 @@ for size in grid_sizes:
     linear_scan_times.append(linear_scan_time)
 
     start_time = time.perf_counter()
-    divconq_result = div_con_search(search_value, 0, size - 1, 0, size - 1, grid)
+    divconq_result = div_con_search(search_value, 0, size - 1, 0, size - 1, grid, 0)
     divconq_time = time.perf_counter() - start_time
     divconq_cells.append(divconq_result[2])
     divconq_times.append(divconq_time)
 
-print("-"*50)
 for item in divconq_cells:
+    print(item)
+
+print("-"*50)
+for item in divconq_times:
     print(item)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
